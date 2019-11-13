0a2b404078a86e5c05e7873663f6f896bda0196d
Nicholas Stone <stone.nicholas@gmail.com>
2019-11-12 22:23:46 -0500

    Added logo

--- bufs/bufs/settings.py
+++ bufs/bufs/settings.py
@@ -118,8 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = '/home/nstone/Desktop/bufs_site/bufs/media'
+#STATIC_ROOT = '/home/nstone/Desktop/bufs_site/bufs/static/'
+
+MEDIA_ROOT = '/home/nstone/Desktop/bufs_site/bufs/media/'
 
 MEDIA_URL = '/media/'
